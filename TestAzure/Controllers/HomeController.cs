42f84b72d6a2e4ea489131fdd60a12e9180e1f19
Puchianu Mirel <mirel_puky_96@ymail.com>
2017-09-18 13:42:01 +0300

    Commit project

---
+++ TestAzure/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestAzure.Models;
+using TestAzure.DBModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using eComplaints.Models.ReportViewModels;
+using TestAzure.Models.ReportViewModels;
+
+namespace TestAzure.Controllers
+{
+    public class HomeController : Controller
+    {
+        eComplaintsCTX ctx;
+
+        public HomeController()
+        {
+            ctx = new eComplaintsCTX();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Originator(string message = null)
+        {
+            ViewBag.Message = message;
+
+            var Areas = ctx.Area.Where(x => x.DepartmentId == 2).ToList();
+            var AreaList = new SelectList(Areas, "Id", "Name");
+            ViewBag.AreaList = AreaList;
+
+
+            var qCategories = ctx.Qcategory.Where(x => x.DepartmentId == 2).ToList();
+            var Categorylist = new SelectList(qCategories, "Id", "Name");
+            ViewBag.CategoryList = Categorylist;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Originator(MOriginatorReport model)
+        {
+            return Originator("Plangere creata cu succes");
+        }
+
+        public JsonResult RetrieveEquipment(int areaId)
+        {
+            var zones = ctx.Zone.Where(arr => arr.AreaId == areaId).ToList();
+
+            List<Equipment> EquipmentList = new List<Equipment>();
+            foreach (var z in zones)
+            {
+                var equipment = ctx.Equipment.Where(arr => arr.ZoneId == z.Id).ToList();
+                foreach (var eq in equipment)
+                {
+                    if (!EquipmentList.Contains(eq))
+                        EquipmentList.Add(eq);
+                }
+            }
+
+            List<EquipmentJSON> json2Send = new List<EquipmentJSON>();
+            foreach (var e in EquipmentList)
+                json2Send.Add(new EquipmentJSON { Id = e.Id, EquipmentName = e.Name });
+
+            return Json(json2Send);
+        }
+
+        public JsonResult RetrieveQuestions(int categoryId)
+        {
+            var Questions = ctx.Question.Where(x => x.CategoryId == categoryId).ToList();
+
+            List<QuestionJSON> json2Send = new List<QuestionJSON>();
+            foreach (var c in Questions)
+                json2Send.Add(new QuestionJSON { Id = c.Id, QuestionName = c.Question1 });
+
+            return Json(json2Send);
+        }
+
+
+    }
+}
